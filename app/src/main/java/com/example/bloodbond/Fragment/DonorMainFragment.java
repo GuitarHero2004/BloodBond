38fc71bb48df0a55339a550369fef8c1164e40ea
Minh <s3979367@rmit.edu.vn>
2024-12-17 17:53:27 +0700

    relocated files into Fragment package

--- app/src/main/java/com/example/bloodbond/Fragment/DonorMainFragment.java
+++ app/src/main/java/com/example/bloodbond/Fragment/DonorMainFragment.java
@@ -1,4 +1,4 @@
-package com.example.bloodbond;
+package com.example.bloodbond.Fragment;
 
 import android.os.Bundle;
 
@@ -8,13 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DonorMapFragment extends Fragment {
+import com.example.bloodbond.R;
+
+public class DonorMainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donor_map, container, false);
+        return inflater.inflate(R.layout.fragment_donor_main, container, false);
     }
-
-
 }
--- app/src/main/java/com/example/bloodbond/Fragment/DonorMapFragment.java
+++ app/src/main/java/com/example/bloodbond/Fragment/DonorMapFragment.java
@@ -1,4 +1,4 @@
-package com.example.bloodbond;
+package com.example.bloodbond.Fragment;
 
 import android.os.Bundle;
 
@@ -8,11 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DonorMainFragment extends Fragment {
+import com.example.bloodbond.R;
+
+public class DonorMapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donor_main, container, false);
+        return inflater.inflate(R.layout.fragment_donor_map, container, false);
     }
+
+
 }
